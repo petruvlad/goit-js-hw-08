ae14beb11fe200a865b045f7e095dc73d4f2b21e
petruvlad <vlad.petrisor31@gmail.com>
2023-12-16 00:10:14 +0200

    5th commit
    adaugare exercitiu 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = new Player(document.getElementById('vimeo-player'));
+const storageKey = 'videoplayer-current-time';
+
+
+const saveCurrentTime = throttle(async () => {
+  try {
+    const currentTime = await vimeoPlayer.getCurrentTime();
+    localStorage.setItem(storageKey, currentTime);
+  } catch (error) {
+    console.error('Error saving current time:', error.message);
+  }
+}, 1000); 
+
+
+vimeoPlayer.on('timeupdate', saveCurrentTime);
+
+async function setPlayerTimeFromStorage() {
+  const storedTime = localStorage.getItem(storageKey);
+  if (storedTime !== null) {
+    try {
+      await vimeoPlayer.setCurrentTime(parseFloat(storedTime));
+    } catch (error) {
+      console.error('Error setting current time:', error.message);
+    }
+  }
+}
+
+window.addEventListener('load', setPlayerTimeFromStorage);
