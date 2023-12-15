eb256b5319d03d12e41929eb02a5c465845a4a9a
petruvlad <vlad.petrisor31@gmail.com>
2023-12-15 15:58:18 +0200

    2th commit
    adaugare exercitiu 1 simpleligthbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('gallery__item');
+
+  listItem.innerHTML = `
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  `;
+
+  gallery.appendChild(listItem);
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  closeText: 'Închide',
+});
