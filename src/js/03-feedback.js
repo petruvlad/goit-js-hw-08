f1766a8225a57dbf069a866cd71a87224067aa30
petruvlad <vlad.petrisor31@gmail.com>
2023-12-16 00:19:15 +0200

    7th commit
    adaugare exercitiu 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const feedbackStateKey = 'feedback-form-state';
+
+
+const saveFormData = throttle(() => {
+  const formData = {
+    email: feedbackForm.elements.email.value,
+    message: feedbackForm.elements.message.value,
+  };
+
+  localStorage.setItem(feedbackStateKey, JSON.stringify(formData));
+}, 500);
+
+window.addEventListener('load', () => {
+  const storedFormData = localStorage.getItem(feedbackStateKey);
+
+  if (storedFormData) {
+    const parsedFormData = JSON.parse(storedFormData);
+    feedbackForm.elements.email.value = parsedFormData.email || '';
+    feedbackForm.elements.message.value = parsedFormData.message || '';
+  }
+});
+
+
+feedbackForm.addEventListener('input', saveFormData);
+
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = {
+    email: feedbackForm.elements.email.value,
+    message: feedbackForm.elements.message.value,
+  };
+
+  console.log('Form Data:', formData);
+
+  localStorage.removeItem(feedbackStateKey);
+});
